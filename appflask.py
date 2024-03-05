ee848561b44d7682ea14d01da18a4663893cef36
celiksoner <snrclk94@outlook.com>
2024-03-05 15:49:15 +0000

    git hooks configured

--- appflask.py
+++ appflask.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World! Hook test6</p>"
+    return "<p>Hello, World! Hook test7</p>"
--- startup.sh
+++ startup.sh
@@ -4,4 +4,5 @@ apt-get install -y python
 apt-get install -y python3-venv
 apt install -y python3-flask
 apt install -y python3-pip
+sudo chmod 700 -R /home/vagrant/.ssh
 #flask run --host=0.0.0.0 --debug
